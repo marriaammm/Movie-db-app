ae8b226c1578d11635dae8d6cd755f17ddcc67aa
marriaammm <mariamz@std.mans.edu.eg>
2025-04-26 06:03:13 +0300

    Update .gitignore to exclude environments directory

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiKey: '022377a2eec98e045a36af9fcdeec4c8', // Register at https://www.themoviedb.org/
+    apiKey: '022377a2eec98e045a36af9fcdeec4c8', 
     imageBaseUrl: 'https://image.tmdb.org/t/p/'
 };
