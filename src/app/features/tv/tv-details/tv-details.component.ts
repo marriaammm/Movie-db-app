401276b8b5010d6a586c41abbb3f0f40a079efcb
marriaammm <mariamz@std.mans.edu.eg>
2025-04-26 22:53:06 +0300

    feat: Implement TV details and list components with responsive design

--- src/app/features/tv/tv-details/tv-details.component.ts
+++ src/app/features/tv/tv-details/tv-details.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieApiService } from '../../../core/services/movie-api.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tv-details',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './tv-details.component.html',
-  styleUrl: './tv-details.component.css'
+  styleUrls: ['./tv-details.component.css']
 })
 export class TvDetailsComponent {
+  private route = inject(ActivatedRoute);
+  public movieApiService = inject(MovieApiService);
 
+  tvShow = toSignal(
+    this.route.paramMap.pipe(
+      switchMap(params => {
+        const id = params.get('id');
+        return id ? this.movieApiService.getTvShowDetails(+id) : [];
+      })
+    )
+  );
 }
--- src/app/features/tv/tv-list/tv-list.component.ts
+++ src/app/features/tv/tv-list/tv-list.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MovieApiService } from '../../../core/services/movie-api.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CommonModule } from '@angular/common';
+import { MediaCardComponent } from '../../../shared/components/media-card/media-card.component';
 
 @Component({
   selector: 'app-tv-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MediaCardComponent],
   templateUrl: './tv-list.component.html',
-  styleUrl: './tv-list.component.css'
+  styleUrls: ['./tv-list.component.css']
 })
 export class TvListComponent {
-
+  private mediaService = inject(MovieApiService);
+  tvShows = toSignal(this.mediaService.getPopularTvShows());
 }
