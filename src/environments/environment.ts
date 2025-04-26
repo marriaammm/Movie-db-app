3066c42f7ee42b8f2b0e5da358a4704249b66aca
marriaammm <mariamz@std.mans.edu.eg>
2025-04-26 06:01:50 +0300

    Add environment configuration file for development

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiKey: '022377a2eec98e045a36af9fcdeec4c8', // Register at https://www.themoviedb.org/
+    imageBaseUrl: 'https://image.tmdb.org/t/p/'
+};
